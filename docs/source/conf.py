a75bb1788d0d40fa940714bb62b7d721adda702e
jadewarren <132283032+jadewarren@users.noreply.github.com>
2024-05-22 13:55:23 -0400

    Change name and author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SlicerDESI'
+copyright = '2024'
+author = 'Amoon Jamzad'
 
 release = '0.1'
 version = '0.1.0'
